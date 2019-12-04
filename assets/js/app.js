983e18382ef8c31309fc5007a08f3658c8c348a9
Toua VA <toua.va45@gmail.com>
2019-12-04 16:37:21 +0100

    Corrections footer resolved

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,7 @@ const $ = require('jquery');
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('bootstrap');
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 require('@fortawesome/fontawesome-free/css/all.min.css');
-
+// eslint-disable-next-line import/no-extraneous-dependencies
 require('@fortawesome/fontawesome-free/js/all.js');
