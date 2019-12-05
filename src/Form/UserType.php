f457d8ceb3cd24d492dafc4b118d029366442125
sdesousa <sylvaindesousa@free.fr>
2019-12-05 14:54:28 +0100

    S01_US43_duty remove useless use

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,7 +7,6 @@ use App\Entity\Duty;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
