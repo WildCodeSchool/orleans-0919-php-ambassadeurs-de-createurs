70c97c5aeb080f781bf6e1020364ffe5c9fe0c07
Cindy Planchenault <cnd.planchenault@gmail.com>
2019-12-05 14:06:14 +0100

    fixture avec table departement ok

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,12 +2,14 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Department;
 use App\Entity\User;
 use Faker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
@@ -20,8 +22,16 @@ class UserFixtures extends Fixture
             $user->setPicture($faker->imageUrl(200, 200, 'fashion'));
             $user->setMail($faker->email);
             $user->setRoles('Ambassadeur');
+
+            $user->setDepartment($this->getReference("00" . rand(1, 7)));
             $manager->persist($user);
         }
         $manager->flush();
+    }
+
+
+    public function getDependencies()
+    {
+        return [DepartmentFixtures::class];
     }
 }
