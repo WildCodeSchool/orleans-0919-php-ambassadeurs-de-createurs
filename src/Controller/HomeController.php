f525c38ca8148ead7c1e67387250dfaeb54aec79
Caroline Richard <rchrdcaroline@gmail.com>
2019-12-05 15:16:21 +0100

    dynamic card

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,10 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('/home/index.html.twig');
+        $ambassadors = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findBy(['roles' => 'ambassadeur']);
+
+        return $this->render('/home/index.html.twig', ['ambassadors' => $ambassadors]);
     }
 }
