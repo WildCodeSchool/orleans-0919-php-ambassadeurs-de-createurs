e956765ada9e366c1cbe7964d56d836ad2ae25ce
Toua VA <toua.va45@gmail.com>
2019-12-10 17:35:25 +0100

    Files about create Category realised

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ * @UniqueEntity(fields={"description"}, message="Un univers avec la même description existe déjà")
  */
 class Category
 {
@@ -20,6 +23,7 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $description;
 
