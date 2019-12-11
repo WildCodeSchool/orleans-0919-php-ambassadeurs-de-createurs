7be277993471e8b8ec548b2c57f02cc942fb9af1
sdesousa <sylvaindesousa@free.fr>
2019-12-11 14:14:41 +0100

    S01_US43 fix remove other branch

--- src/DataFixtures/DepartmentFixtures.php
+++ src/DataFixtures/DepartmentFixtures.php
@@ -2,42 +2,30 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
 use App\Entity\Department;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class DepartmentFixtures extends Fixture
 {
-
-    const CSV_FILE = 'assets/misc/departements-only-france.csv';
-
+    const DEPARTMENT = [
+        "001" => "Ain",
+        "002" => "Aisne",
+        "003" => "Allier",
+        "004" => "Basses-Alpes",
+        "005" => "Hautes-Alpes",
+        "006" => "Alpes-Maritimes",
+        "007" => "Ardèche",
+    ];
     public function load(ObjectManager $manager)
     {
-        $departments = $this->readFileDepartment(self::CSV_FILE);
-        array_pop($departments);
-        $loopIndex = 1;
-        foreach ($departments as $departmentCSV) {
+        foreach (self::DEPARTMENT as $number => $name) {
             $department = new Department();
-            $department->setCode($departmentCSV[1]);
-            $department->setName($departmentCSV[2]);
-            $this->addReference('department_'.$loopIndex, $department);
-            $loopIndex++;
+            $department->setCode($number);
+            $department->setName($name);
+            $this->addReference($number, $department);
             $manager->persist($department);
         }
         $manager->flush();
-    }
-
-    public function readFileDepartment(string $file): array
-    {
-        $departments = [];
-        $csvFile = fopen($file, "r");
-        if ($csvFile != false) {
-            while (!feof($csvFile)) {
-                $departments[] = fgetcsv($csvFile, 1000, ",");
-            }
-        }
-        fclose($csvFile);
-        return $departments;
     }
 }
