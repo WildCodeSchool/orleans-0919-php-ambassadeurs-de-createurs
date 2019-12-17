46704d2294f29631b752e4dc70d19f47d35f5802
sdesousa <sylvaindesousa@free.fr>
2019-12-17 16:39:56 +0100

    S03_US15 fix icon compatibility with yarn

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,12 +5,17 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import * as L from 'leaflet';
+// eslint-disable-next-line import/no-unresolved
+import 'leaflet-defaulticon-compatibility';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
+import { GestureHandling } from 'leaflet-gesture-handling';
+import 'leaflet-gesture-handling/dist/leaflet-gesture-handling.css';
+
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/app.scss');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// eslint-disable-next-line import/no-extraneous-dependencies,no-unused-vars
-const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -21,8 +26,9 @@ require('@fortawesome/fontawesome-free/css/all.min.css');
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('@fortawesome/fontawesome-free/js/all.js');
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-require('leaflet');
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// eslint-disable-next-line import/no-extraneous-dependencies,no-unused-vars
+const $ = require('jquery');
 
 // eslint-disable-next-line import/no-unresolved
 require('./map.js');
--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,9 @@
-const map = L.map('map').setView([48.866667, 2.333333], 6);
+const map = L.map('map', { gestureHandling: true }).setView([48.866667, 2.333333], 6);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 12,
-    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+
 }).addTo(map);
 L.control.scale().addTo(map);
-
-let marker = L.marker([48.866667, 2.333333]).addTo(map);
+L.marker([48.866667, 2.333333]).addTo(map);
