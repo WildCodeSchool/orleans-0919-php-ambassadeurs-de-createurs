9d65466cf0512920ecd4764c01b701b7af027135
sdesousa <sylvaindesousa@free.fr>
2019-12-17 08:34:32 +0100

    S03_US15 map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,2 +1,8 @@
 console.log('test');
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([51.505, -0.09], 10);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(map);
