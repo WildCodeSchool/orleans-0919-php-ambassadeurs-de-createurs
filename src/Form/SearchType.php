a4775b1f4a5466f949d95701aafed36f6914694c
Cindy Planchenault <cnd.planchenault@gmail.com>
2019-12-27 15:26:25 +0100

    mise en place filtre statut

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Department;
+use App\Entity\Duty;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -26,6 +27,13 @@ class SearchType extends AbstractType
                 'class' => Category::class,
                 'choice_label' => 'description',
                 'choice_value' => 'description',
+                'required' => false,
+                'label' => false,
+            ])
+            ->add('duty', EntityType::class, [
+                'class' => Duty::class,
+                'choice_label' => 'name',
+                'choice_value' => 'name',
                 'required' => false,
                 'label' => false,
             ])
