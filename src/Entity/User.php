6d43e4f35f3917dfe31dc070f7890ea82600f118
Steven Bédu <steven.bedu@gmail.com>
2019-12-28 12:05:59 +0100

    message uniqueEntity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @UniqueEntity(fields={"mail"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"mail"}, message="Il y a déjà un compte avec cette adresse mail.")
  */
 class User implements UserInterface
 {
