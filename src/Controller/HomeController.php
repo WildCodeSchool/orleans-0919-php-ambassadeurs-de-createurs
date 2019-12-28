c08177f10c69187cf3ed77420e740576f14e8ba0
sdesousa <sylvaindesousa@free.fr>
2019-12-28 15:09:35 +0100

    S03_US15

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,9 @@ use App\Service\CoordinateService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class HomeController extends AbstractController
 {
@@ -21,20 +24,27 @@ class HomeController extends AbstractController
     public function index(CoordinateService $coordinateService): Response
     {
         $roles = User::ROLES;
+        /** @var User[] A user */
         $ambassadors = $this->getDoctrine()
             ->getRepository(User::class)
             ->findBy(['roles' => $roles['Ambassadeur']]);
-        $cards = array_slice($ambassadors, count($ambassadors)-self::NB_CARDS, self::NB_CARDS);
+        $ambassadorCards = array_slice($ambassadors, count($ambassadors)-self::NB_CARDS, self::NB_CARDS);
+
+//        $encoders = [new JsonEncoder()];
+//        $normalizers = [new ObjectNormalizer()];
+//        $serializer = new Serializer($normalizers, $encoders);
 
         $coordinates = [];
-//        foreach ($ambassadors as $ambassador) {
-//            $coordinates[$ambassador->getId()] = $coordinateService
-//                ->getCoordinates($ambassador->getCity());
-//        }
+        foreach ($ambassadors as $ambassador) {
+            if (!is_null($ambassador->getCity())) {
+                $coordinates[$ambassador->getId()] = $coordinateService
+                    ->getCoordinates($ambassador->getCity());
+            }
+        }
 
         return $this->render('/home/index.html.twig', [
             'ambassadors' => $ambassadors,
-            'cards' => $cards,
+            'ambassadorCards' => $ambassadorCards,
             'coordinates' => $coordinates,
         ]);
     }
