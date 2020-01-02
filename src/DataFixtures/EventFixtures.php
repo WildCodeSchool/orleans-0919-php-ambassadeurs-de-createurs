939bfbeb8174c3adc97cf16681f4c323824c62b7
Caroline Richard <rchrdcaroline@gmail.com>
2020-01-02 11:14:25 +0100

    fixture and card okay

--- src/DataFixtures/EventFixtures.php
+++ src/DataFixtures/EventFixtures.php
@@ -6,10 +6,11 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use App\Entity\Event;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker;
 
-class EventFixtures extends Fixture
+class EventFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
@@ -17,11 +18,22 @@ class EventFixtures extends Fixture
         for ($i = 0; $i < 80; $i++) {
             $blog = new Event();
             $blog->setPlace($faker->city);
-            $blog->setUser($this->getReference('USER_' . rand(0, 20)));
+            $blog->setUser($this->getReference('user_' . rand(0, 19)));
             $blog->setDescription($faker->sentence(6));
             $blog->setDateTime($faker->dateTime);
             $manager->persist($blog);
         }
         $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return [ UserFixtures::class];
     }
 }
