35e65b941d1f1c4240ef6f2e052f3ff446c08116
sdesousa <sylvaindesousa@free.fr>
2020-01-02 11:59:54 +0100

    S03_US15 fix API call

--- src/Service/CoordinateService.php
+++ src/Service/CoordinateService.php
@@ -9,7 +9,8 @@ class CoordinateService
     public function getCoordinates(string $city): array
     {
         $client = HttpClient::create();
-        $response = $client->request('GET', 'https://api-adresse.data.gouv.fr/search/?q='.$city);
+        $url = 'https://api-adresse.data.gouv.fr/search/?q='.$city.'&type=municipality';
+        $response = $client->request('GET', $url);
         $content = $response->toArray();
         $coordinates = $content['features'][0]['geometry']['coordinates'];
         return $coordinates;
