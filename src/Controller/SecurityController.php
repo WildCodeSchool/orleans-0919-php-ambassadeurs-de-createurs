5d511726f7ee24bec0b542d72ed7df2b69e180ac
Steven Bédu <steven.bedu@gmail.com>
2020-01-07 16:52:07 +0100

    update home index

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,12 +24,13 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-             return $this->redirectToRoute('home_index');
+            return $this->redirectToRoute('home_index');
         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
+
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
