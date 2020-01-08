af2f26d0fdb22ee67843b7d81cb0c63b71cc2953
sdesousa <sylvaindesousa@free.fr>
2020-01-08 11:13:01 +0100

    S04_US09 remove aticleTAgRepository commented code

--- src/Repository/ArticleTagRepository.php
+++ src/Repository/ArticleTagRepository.php
@@ -18,33 +18,4 @@ class ArticleTagRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ArticleTag::class);
     }
-
-    // /**
-    //  * @return ArticleTag[] Returns an array of ArticleTag objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?ArticleTag
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
