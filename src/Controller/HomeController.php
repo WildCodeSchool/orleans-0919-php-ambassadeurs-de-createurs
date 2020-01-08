7b64f9df0cd03a600c3667303290dad2b0fe0c31
sdesousa <sylvaindesousa@free.fr>
2020-01-08 15:04:45 +0100

    S03_US14 css tabs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends AbstractController
         $ambassadorCards = array_slice($ambassadors, count($ambassadors)-self::NB_CARDS, self::NB_CARDS);
 
         $context = [
-            AbstractNormalizer::IGNORED_ATTRIBUTES => ['users', 'user', 'eventsSponsored'],
+            AbstractNormalizer::IGNORED_ATTRIBUTES => ['users', 'user', 'sponsoredEvents'],
         ];
         $ambassadorsJson = $serializer->serialize($ambassadors, 'json', $context);
 
