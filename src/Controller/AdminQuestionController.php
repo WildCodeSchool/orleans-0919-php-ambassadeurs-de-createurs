b3403d4127c706e25aef9b9c0669d8c6eddd1149
sdesousa <sylvaindesousa@free.fr>
2020-01-13 16:34:03 +0100

    S05_US10 edit

--- src/Controller/AdminQuestionController.php
+++ src/Controller/AdminQuestionController.php
@@ -68,7 +68,7 @@ class AdminQuestionController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
-
+            $this->addFlash('success', 'Votre question a été modifiée');
             return $this->redirectToRoute('admin_question_index');
         }
 
