24fb00c137ffcf7d05b20eb55e2925f315ee79c1
sdesousa <sylvaindesousa@free.fr>
2020-01-13 13:52:41 +0100

    S05_US10 tables and fixtures

---
+++ src/Entity/Question.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\QuestionRepository")
+ */
+class Question
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $question;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $answer;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\QuestionCategory", inversedBy="questions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuestion(): ?string
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(string $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getAnswer(): ?string
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(string $answer): self
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    public function getCategory(): ?QuestionCategory
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?QuestionCategory $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
