ee956d5c1b4a71a664c14559b7bd15db12fe194e
Cindy Planchenault <cnd.planchenault@gmail.com>
2020-01-15 14:41:24 +0100

    rename controller et template

--- src/Controller/CompagnyController.php
+++ src/Controller/CompagnyController.php
@@ -5,13 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ConceptController extends AbstractController
+/**
+ * @Route("compagny")
+ */
+class CompagnyController extends AbstractController
 {
     /**
      * @Route("/concept", name="app_concept")
      */
     public function show()
     {
-        return $this->render('concept/index.html.twig');
+        return $this->render('compagny/concept_index.html.twig');
     }
 }
