e81cf5b84e8dd4e3367d1cea6e57943c8c705b12
sdesousa <sylvaindesousa@free.fr>
2020-01-20 15:54:32 +0100

    S05_profil layout

---
+++ assets/js/gallery.js
@@ -0,0 +1,19 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $('.myImg');
+var modalImg = $("#img01");
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
