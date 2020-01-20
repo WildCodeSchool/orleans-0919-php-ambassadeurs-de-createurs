1c502507897aa5e6e6f2a9dbc189aac849e8d81b
sdesousa <sylvaindesousa@free.fr>
2020-01-20 16:17:33 +0100

    S05_profil travis

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,19 +1,22 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+const modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = $('.myImg');
-var modalImg = $("#img01");
-$('.myImg').click(function(){
-    modal.style.display = "block";
-    var newSrc = this.src;
+// eslint-disable-next-line no-unused-vars,no-undef
+const img = $('.myImg');
+// eslint-disable-next-line no-undef
+const modalImg = $('#img01');
+// eslint-disable-next-line no-undef,func-names
+$('.myImg').click(function () {
+    modal.style.display = 'block';
+    const newSrc = this.src;
     modalImg.attr('src', newSrc);
 });
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+span.onclick = function () {
+    modal.style.display = 'none';
+};
