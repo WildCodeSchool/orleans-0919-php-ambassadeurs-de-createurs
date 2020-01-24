c01ef1cb339ef2cd2ec28d305f15cad571626dfb
Caroline Richard <rchrdcaroline@gmail.com>
2020-01-24 17:41:57 +0100

    fixes for travis

--- assets/js/adminforms.js
+++ assets/js/adminforms.js
@@ -1,6 +1,8 @@
-let checkboxes = document.getElementsByClassName('onoffswitch-checkbox');
-for(checkbox of checkboxes) {
-    checkbox.addEventListener('change', function(event) {
+const checkboxes = document.getElementsByClassName('onoffswitch-checkbox');
+// eslint-disable-next-line no-undef,no-restricted-syntax
+for (checkbox of checkboxes) {
+    // eslint-disable-next-line no-undef
+    checkbox.addEventListener('change', (event) => {
         event.target.form.submit();
     });
-};
+}
