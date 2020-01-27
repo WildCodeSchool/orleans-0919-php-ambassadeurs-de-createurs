f2ea41ed84ea7203cea4f4be000be14a2d02e9e6
sdesousa <sylvaindesousa@free.fr>
2020-01-27 08:58:36 +0100

    S06_fix fixtures faq

--- src/DataFixtures/QuestionFixtures.php
+++ src/DataFixtures/QuestionFixtures.php
@@ -3,28 +3,34 @@
 namespace App\DataFixtures;
 
 use App\Entity\Question;
+use App\Repository\QuestionCategoryRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker;
 
-class QuestionFixtures extends Fixture implements DependentFixtureInterface
+class QuestionFixtures extends Fixture
 {
+
+    private $categoryRepository;
+    /**
+     * QuestionFixtures constructor.
+     */
+    public function __construct(QuestionCategoryRepository $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
+        $categories = $this->categoryRepository->findAll();
         for ($i = 0; $i < 20; $i++) {
             $question = new Question();
             $question->setQuestion($faker->sentence);
             $question->setAnswer($faker->paragraph(12));
-            $question->setCategory($this->getReference('questionCategory_' . rand(0, 2)));
+            $question->setCategory(array_rand($categories));
             $manager->persist($question);
         }
         $manager->flush();
-    }
-
-    public function getDependencies()
-    {
-        return [QuestionCategoryFixtures::class];
     }
 }
